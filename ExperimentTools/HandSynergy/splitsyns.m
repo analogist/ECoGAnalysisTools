888eb4d429e1bc65b6d363a79a7aadac536d72c3
James Wu <jiwu@cs.washington.edu>
2015-02-08 14:28:52 -0800

    merge splitsyns critical kron fix

--- ExperimentTools/HandSynergy/splitsyns.m
+++ ExperimentTools/HandSynergy/splitsyns.m
@@ -3,7 +3,7 @@ function [ diff_synergy_split ] = splitsyns( diffsynergy )
     if(size(diffsynergy, 1) < size(diffsynergy, 2))
         error('Not a synergy matrix? Should be [t x syns]');
     end
-    diff_synergy_split = repmat(diffsynergy, [1 2]);
+    diff_synergy_split = kron(diffsynergy, [1 1]);
     
     for iter = 1:size(diff_synergy_split, 2)        
         tempsyn = diff_synergy_split(:, iter);
