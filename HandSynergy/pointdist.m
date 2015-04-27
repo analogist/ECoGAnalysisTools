9ca7ab4e0d1d96eb1238dbd061d87adadd5aa90d
James Wu <jiwu@cs.washington.edu>
2015-04-26 23:22:31 -0700

    add linedist, pointdist metrics

--- HandSynergy/pointdist.m
+++ HandSynergy/pointdist.m
@@ -1,9 +1,6 @@
-function [ distances ] = rmsdist( series1, series2 )
+function [ distances ] = pointdist( series1, series2 )
 %RMSDIST Summary of this function goes here
 %   Detailed explanation goes here
-    if(nargin ~= 2)
-        error('Matrix must be 2xn or nx2')
-    end
 
     if( ~isequal(size(series1),size(series2)) )
         error('Two matrices do not match in size')
