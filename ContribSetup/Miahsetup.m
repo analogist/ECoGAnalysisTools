86c7e79feed7f2921d38fe2fdbd83ddcbc7a0c1d
James Wu <jiwu@cs.washington.edu>
2015-08-17 22:40:52 -0700

    add TFA to Miah codebase

--- ContribSetup/Miahsetup.m
+++ ContribSetup/Miahsetup.m
@@ -2,6 +2,7 @@ function [ ] = Miahsetup( appdir )
 %MIAHSETUP Setup path for Miah's tools
     addpath([appdir '/Miah/DataPrep']);
     addpath([appdir '/Miah/SigAnal']);
+    addpath([appdir '/Miah/SigAnal/TFA']);
     addpath([appdir '/Miah/Visualization']);
     addpath([appdir '/Miah/Visualization/recon']);
     addpath([appdir '/Miah/External/spm8']);
