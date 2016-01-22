9f66379f6a03ba2708f592f4d06e66b267d317ae
James Wu <jiwu@cs.washington.edu>
2016-01-21 17:32:07 -0800

    add variable startpt to phaseshuffle()

--- Common/phaseshuffle.m
+++ Common/phaseshuffle.m
@@ -1,23 +1,30 @@
-function [ shuffledata ] = phaseshuffle( indata, interp )
+function [ shuffledata ] = phaseshuffle( indata, startpt )
 %PHASESHUFFLE Summary of this function goes here
 %   Detailed explanation goes here
 
-    if(~exist('interp', 'var'))
+%    if(~exist('interp', 'var'))
+    if(~exist('startpt', 'var'))
         datadiff = diff(indata);
 %         diffshuff = datadiff(randperm(length(datadiff)));
         diffshuff = datadiff(randi(length(datadiff), length(datadiff), 1));
         shuffledata = cumsum([indata(1); diffshuff]);
     else
-        if(interp == 1)
-            datadiff = diff(glove_interp(indata));
-            diffshuff = datadiff(randperm(length(datadiff)));
-            shuffledata = cumsum([indata(1); diffshuff]);
-        elseif(interp == 2)
-            datadiff = diff(glove_interp(indata));
-            diffshuff = datadiff(randi(length(datadiff), length(datadiff), 1));
-            shuffledata = cumsum([indata(1); diffshuff]);
-        end
+        datadiff = diff(indata);
+        diffshuff = datadiff(randi(length(datadiff), length(datadiff), 1));
+        shuffledata = cumsum([indata(1); diffshuff]);
     end
+
+%    else
+%        if(interp == 1)
+%            datadiff = diff(glove_interp(indata));
+%            diffshuff = datadiff(randperm(length(datadiff)));
+%            shuffledata = cumsum([indata(1); diffshuff]);
+%        elseif(interp == 2)
+%            datadiff = diff(glove_interp(indata));
+%            diffshuff = datadiff(randi(length(datadiff), length(datadiff), 1));
+%            shuffledata = cumsum([indata(1); diffshuff]);
+%        end
+%    end
 
 end
 
