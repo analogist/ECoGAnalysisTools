9cec70d797bf5ab4af853a360944c8c08b8c3ea1
James Wu <jiwu@cs.washington.edu>
2016-02-10 14:43:36 -0800

    make unity() multidimensional

--- Common/unity.m
+++ Common/unity.m
@@ -1,7 +1,10 @@
 function [ output ] = unity( input )
 %UNITY Summary of this function goes here
 %   Detailed explanation goes here
-    output = (input - min(input))/range(input);
+    output = zeros(size(input));
+    for i = 1:size(input, 2)
+        output(:, i) = (input(:, i) - min(input(:, i)))/range(input(:, i));
+    end
 
 end
 
