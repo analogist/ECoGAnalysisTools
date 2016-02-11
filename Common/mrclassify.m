9c5aca5a5b493ee88460f42838912ed049af8034
James Wu <jiwu@cs.washington.edu>
2016-02-10 16:43:38 -0800

    replace mrclassify/crossval to be flexible across algorithms

--- Common/mrclassify.m
+++ Common/mrclassify.m
@@ -1,11 +1,23 @@
-function [sumout] = mrclassify(xtr, ytr, xte, yte)
+function [yfit] = mrclassify(XTRAIN, ytrain, XTEST, mrparams, classifier)
 %MRCLASSIFY Summary of this function goes here
 %   Detailed explanation goes here
 
-    crit = mrmr_mid_d(xtr, uint8(ytr), 10);
+    if(~exist('classifier', 'var'))
+        classifier = 'LDA';
+    end
+    if(~exist('mrparams', 'var'))
+        mrparams = 10;
+    end
+
+    crit = mrmr_mid_d(XTRAIN, uint8(ytrain), mrparams);
+    XTRAINprune = XTRAIN(:, crit);
+    XTESTprune = XTEST(:, crit);
     
-    xtrprune = xtr(:, crit);
-    xteprune = xte(:, crit);
-    sumout = sum(yte==classify(xteprune, xtrprune, ytr));
+    switch classifier
+        case 'LDA'
+            yfit = classify(XTESTprune, XTRAINprune, ytrain);
+        otherwise
+           error('classifier error') 
+    end
 end
 
