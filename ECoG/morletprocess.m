cb64d31bb03b8b597ca4769a1daa281dc8e4f559
James Wu <jiwu@cs.washington.edu>
2016-04-26 15:43:42 -0700

    separate truncbindata() as function

--- ECoG/morletprocess.m
+++ ECoG/morletprocess.m
@@ -1,4 +1,4 @@
-function [ powerout, phaseangle ] = morletprocess( inputs, fs, time_res, use_lmp )
+function [ f, t, powerout ] = morletprocess( inputs, fs, time_res, use_lmp )
 %MORLETPROCESS Processing ECoG spectrum using analytic/nonanalytic Morlet
 %wavelets in Matlab cwtft(). Outputs spectral power.
 %   [POWEROUT, PHASEANGLE] = MORLETPROCESS(INPUTS, FS, TIME_RES, USE_LMP)
@@ -19,49 +19,65 @@ function [ powerout, phaseangle ] = morletprocess( inputs, fs, time_res, use_lmp
 %   "local motor potential".
 %
 
+    if(~exist('use_lmp', 'var'))
+        use_lmp = false;
+    end
+    
+    % initialize dt and bins
     dt = 1/fs;
-    binby = round(fs*time_res);
-    truncateby = mod(length(inputs), binby)
+    binsize = round(fs*time_res);
+    truncateby = mod(length(inputs), binsize);
+    
+    % establish scales, frequencies, and time
     scales = helperCWTTimeFreqVector(2, 200, 3/pi, dt, 8);
-    cwty = cwtft({sig(:, 1), dt},'wavelet','morlex','scales',scales,'padmode','symw');
-    f = cwty.frequencies;
-    t = binby*dt/2:binby*dt:trueend*dt;
+    f = (3/pi)./scales;
+    t = (binsize*dt/2):(binsize*dt):((length(inputs)-truncateby)*dt);
 
-    siglmp = zeros(1, length(t), size(sig, 2));
-    sigfft = zeros(length(f), length(t), size(sig, 2));
-    for i = 1:size(sig, 2)
-        cwty = cwtft({sig(:, i), dt},'wavelet','morlex','scales',scales,'padmode','symw');
-        cfsprebin = abs(cwty.cfs);
-        cfsprebin = cfsprebin(:, 1:trueend);
-        cfsbinning = cfsprebin';
-        cfsbinning = reshape(cfsbinning, binby, size(cfsbinning, 1)/binby, size(cfsbinning, 2));
-        cfsbinning = squeeze(mean(cfsbinning, 1));
-        cfsbinning = cfsbinning';
-        sigfft(:, :, i) = cfsbinning;
+    powerout = zeros(length(f), length(t), size(inputs, 2));
+    if(use_lmp)
+        siglmp = zeros(1, length(t), size(inputs, 2));
+    end
+    
+    for i = 1:size(inputs, 2)
+        cwty = cwtft({inputs(:, i), dt},'wavelet','morlex','scales',scales,'padmode','symw');
+        powerout(:, :, i) = truncbindata(abs(cwty.cfs), truncateby, binsize);
 
         if(use_lmp)
-            lmpprebin = glove_smooth(sig(:, i), 1220.7, 1, 3);
-            lmpprebin = lmpprebin(1:trueend, :);
-            lmpbinning = reshape(lmpprebin, binby, size(lmpprebin, 1)/binby, size(lmpprebin, 2));
-            lmpbinning = squeeze(mean(lmpbinning, 1));
-            lmpbinning = lmpbinning';
-            siglmp(:, :, i) = lmpbinning;
+            lmpprebin = glove_smooth(inputs(:, i), 1220.7, 1, 3);
+            siglmp(:, :, i) = truncbindata(lmpprebin, truncateby, binsize);
         end
-        disp(i);
     end
-    clear cfsprebin;
-    clear cfsbinning;
 
     if(use_lmp)
-        sigfft = cat(1,sigfft, siglmp);
+        powerout = cat(1,powerout, siglmp);
         f(end+1) = 0;
-
-        clear lmpprebin;
-        clear lmpbinning;
-
-        if(freemem)
-            clear siglmp;
-        end
     end
+end
+
+function [ postbin ] = truncbindata( prebin, truncateby, binsize, forcedim )
+% This function bins input along time, and assumes time is the longer dim.
+% If this is not true, set forcedim = 1 or 2
+%
+% When output, time is along dim 2
+    if(~exist('forcedim', 'var'))
+        forcedim = [];
+    end
+    if(isempty(forcedim))
+        if(size(prebin, 2) > size(prebin, 1))
+            prebin = prebin';
+        end
+    elseif(forcedim == 2)
+        prebin = prebin';
+    elseif(forcedim ~= 1)
+        error('dimensional error on binning');
+    else
+        error('dimensional parameter error on binning');
+    end
+
+    binningtemp = prebin(1:end-truncateby, :);
+    binningtemp = reshape(binningtemp, binsize, size(binningtemp, 1)/binsize, size(binningtemp, 2));
+    binningtemp = squeeze(mean(binningtemp, 1));
+    
+    postbin = binningtemp';
 end
 
